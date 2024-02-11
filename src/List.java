53e96fe8df26f4280663ca246478fe5871851e21
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-02-11 10:03:14 +0530

    list collections

--- src/List.java
+++ src/List.java
@@ -1,22 +1,21 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.ListIterator;
 
-public class ArrayListFramework {
+public class List {
     public static void main(String[] args) {
-        List<Integer> list01 = new ArrayList<>();
+        java.util.List<Integer> list01 = new ArrayList<>();
         list01.add(1);
         list01.add(2);
         list01.add(3);
         System.out.println(list01);
 
-        List<Integer> list02 = new ArrayList<>(list01);
+        java.util.List<Integer> list02 = new ArrayList<>(list01);
         //duplicate and make a new copy using constructor
         list02.add(4);
         System.out.println(list02);
 
-        List<Integer> list03 = list02.subList(1,3);
+        java.util.List<Integer> list03 = list02.subList(1,3);
         //this won't make a duplicate arraylist
         System.out.println(list03);
 
